d464b5b87145368a87ab57d22b5f8bbb91d4fee2
Minal1205 <minalamlani@gmail.com>
2022-10-28 20:45:05 +0530

    updated

--- 7.c
+++ 7.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 //to check the given number is even or odd using bitwise operator
 int main(){
+    int x;
+  printf("Enter Number\n");
+  scanf("%d",&x);
+  (x&1==1)?printf("number is odd"):printf("number is even");
     
     return 0;
 }
